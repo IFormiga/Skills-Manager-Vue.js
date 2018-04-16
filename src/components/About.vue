b8c7f6e663c1f4283fabba9bf199dd96ce66be69
Igor Formiga <igorformigaa@gmail.com>
2018-04-16 08:08:36 -0300

    Update About.vue

--- src/components/About.vue
+++ src/components/About.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
-    <p>I'm a programmer with 3 years of experience and everything will be okay while you have me on your pay list.</p>
+    <p>This is a about me page written in Vue.js.</p>
   </div>
 </template>
